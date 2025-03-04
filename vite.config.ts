a6106d930a3062e140a3983b5ac207263802a070
Diego Rojas <devup2332@gmail.com>
2025-03-04 14:32:15 -0500

    master: Edited package

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,40 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+import dts from "vite-plugin-dts";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
+	plugins: [
+		react(),
+		tailwindcss(),
+		dts({
+			insertTypesEntry: true,
+			outDir: "dist",
+			tsconfigPath: "./tsconfig.app.json",
+		}),
+	],
 	resolve: {
 		alias: {
-			"@": "/src",
+			"@": path.resolve(__dirname, "src"),
+		},
+	},
+	build: {
+		lib: {
+			entry: path.resolve(__dirname, "src/index.ts"),
+			name: "salesforce-components",
+			formats: ["es", "umd"],
+			fileName: (format) => `index.${format}.js`,
+		},
+		rollupOptions: {
+			external: ["react", "react-dom"], // Excluir dependencias de la build
+			output: {
+				globals: {
+					react: "React",
+					"react-dom": "ReactDOM",
+				},
+			},
 		},
 	},
 });
